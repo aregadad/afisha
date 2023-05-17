0b58baa0122c19060b3d5294b372eeaea83250b2
aregadad <biafleks@gmail.com>
2023-05-17 13:16:17 +0300

    Add model for images

--- places/models.py
+++ places/models.py
@@ -9,3 +9,11 @@ class Place(models.Model):
 
     def __str__(self):
         return self.title
+
+class Image(models.Model):
+    image = models.ImageField(blank=True, null=True)
+    position = models.IntegerField()
+    place = models.ForeignKey(Place, models.PROTECT, related_name='images', null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.position} {self.place}'
