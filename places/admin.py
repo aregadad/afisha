da5f12c2979bb40a5d9f04c4e75a9bd90abdfb11
aregadad <biafleks@gmail.com>
2023-05-18 15:57:36 +0300

    Add sorting to ImageInline

--- places/admin.py
+++ places/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Place, Image
 from django.utils.html import format_html
+from adminsortable2.admin import SortableStackedInline, SortableAdminBase
 
-class ImageInline(admin.TabularInline):
+
+class ImageInline(SortableStackedInline):
     def get_preview(self, obj):
         return format_html(f'<img src="{obj.image.url}" height=200px>')
 
@@ -12,7 +14,7 @@ class ImageInline(admin.TabularInline):
 
 
 @admin.register(Place)
-class PlaceAdmin(admin.ModelAdmin):
+class PlaceAdmin(SortableAdminBase, admin.ModelAdmin):
     inlines = (ImageInline, )
 
 
--- places/models.py
+++ places/models.py
@@ -14,8 +14,11 @@ class Place(models.Model):
 
 class Image(models.Model):
     image = models.ImageField(blank=True, null=True)
-    position = models.IntegerField()
+    position = models.PositiveIntegerField(default=0, db_index=True)
     place = models.ForeignKey(Place, models.PROTECT, related_name='images', null=True, blank=True)
 
     def __str__(self):
         return f'{self.position} {self.place}'
+
+    class Meta:
+        ordering = ('position', )
