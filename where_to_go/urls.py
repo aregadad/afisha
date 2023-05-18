2a91f8a6ad3640eb887549f1e322dcf06364e7ac
aregadad <biafleks@gmail.com>
2023-05-18 13:47:48 +0300

    Replace test json data with data from db

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 from where_to_go import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.show_index),
     path('admin/', admin.site.urls),
+    path('places/<int:place_id>/', views.show_place),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404
 from places.models import Place
+from django.urls import reverse
 
 
-def index(request):
+def show_index(request):
     geo = {
         'type': 'FeatureCollection',
         'features':
@@ -18,10 +21,26 @@ def index(request):
                 {
                     'title': place.title,
                     'placeId': place.place_id,
-                    'detailsUrl': f'static/places/{place.place_id}.json'
+                    'detailsUrl': reverse(show_place, args=[place.id])
                 }
             } for place in Place.objects.all()
         ]
     }
     context = {'geo': geo}
     return render(request, 'index.html', context)
+
+
+def show_place(request, place_id):
+    place = get_object_or_404(Place, id=place_id)
+    content = {
+        'title': place.title,
+        'imgs': [image.image.url for image in place.images.all()],
+        'description_short': place.description_short,
+        'description_long': place.description_long,
+        'coordinates':
+        {
+            'lng': place.lng_coord,
+            'lat': place.lat_coord
+        }
+    }
+    return JsonResponse(content, safe=False, json_dumps_params={'ensure_ascii': False, 'indent': 2})
